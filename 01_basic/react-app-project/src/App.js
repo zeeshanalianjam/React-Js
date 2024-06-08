4abb5d371b1040b927754dedca3ed1a6ef43509f
= <=>
2024-06-08 12:18:49 +0500

    first commit

---
+++ 01_basic/react-app-project/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>Welcome to React App</h1>
+  );
+}
+
+export default App;
---
+++ 01_basic/vite-project/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+
+  return (
+   <h1>Chai aur React with Vite | ZAA </h1>
+  )
+}
+
+export default App
