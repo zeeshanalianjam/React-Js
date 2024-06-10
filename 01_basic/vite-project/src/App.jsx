7fc682847fd0ddddbe22ab0dea8177ccdbaf30c3
= <=>
2024-06-10 16:48:22 +0500

    create custome react libaray part 2

--- 01_basic/vite-project/src/App.jsx
+++ 01_basic/vite-project/src/App.jsx
@@ -1,10 +1,12 @@
 import Greetings from './Greetings'
 
 function App() {
+  const username = 'chai aur code'
 
+  // {} ---> expression ----> evalvated expression
   return (
     <>
-   <h1>Chai aur React with Vite | ZAA </h1>
+   <h1>Chai aur React with Vite | ZAA {username} </h1> 
     <Greetings />
     </>
   )
--- 01_basic/vite-project/src/main.jsx
+++ 01_basic/vite-project/src/main.jsx
@@ -8,8 +8,30 @@ function MyApp(){
   )
 }
 
+
+// let reactObj = {
+//   type : 'a',
+//   props : {
+//       href : 'https://www.google.com',
+//       target : '_blank'
+//   },
+//   children : 'Google'
+// }
+
+const myVar = "Zeeshan Ali"
+
+const otherWay = React.createElement(
+  'a' , {'href' : 'https://www.google.com' , 'target' : '_blank'},
+  'Google',
+  React.createElement('br'),
+  myVar
+
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <MyApp />
+    <App />
+    {otherWay}
   </React.StrictMode>,
 )
