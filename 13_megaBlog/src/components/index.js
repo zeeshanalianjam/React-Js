a169f864b40017b2a327d752bb79d2ba82221f36
zeeshan ali anjam <zeeshanalianjam49@gmail.com>
2024-07-13 13:20:34 +0500

    index file updaed in components

--- 13_megaBlog/src/components/index.js
+++ 13_megaBlog/src/components/index.js
@@ -3,5 +3,7 @@ import Footer from "./Footer/Footer";
 import Container from './container/Container'
 import Logo from "./Logo";
 import LogoutBtn from './Header/LogoutBtn'
+import Button from './Button'
+import Input from './Input'
 
-export { Header, Footer, Container, Logo, LogoutBtn };
+export { Header, Footer, Container, Logo, LogoutBtn, Button, Input };
